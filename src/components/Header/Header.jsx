289e3ec49e4170f1862461e44e3fe5c8a8e6ba5e
Atta ur Rehman <attaurr786@outlook.com>
2023-08-12 14:39:40 +0500

    Header completed

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,20 +1,20 @@
 import React from 'react'
+
 import './Header.css'
 function Header() {
   return (
    <section className='h-wrapper'>
-    <div className='flexCenter padding innerWidth h-container'>
+    <div className='flexCenter paddings innerWidth h-container'>
     <img src="logo.png" alt="LOGO"  width={100}/>
-    </div>
-<div className='flex-Center h-menu'>
-    <a href="">Home</a>
-    <a href="">Home</a>
-    <a href="">Home</a>
-    <a href="">Home</a>
-    <a href="">Home</a>
-    <button className='button'>Contact Us</button>
+<div className='flexCenter h-menu'>
+    <a href="">Residencies</a>
+    <a href="">values</a>
+    <a href="">contact us</a>
+    <a href="">Get Started</a>
+    <button className='button'>
+      <a href="">contact us</a></button>
 </div>
-
+</div>
    </section>
   )
 }

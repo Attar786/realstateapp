0c337cb7ecb2195501c300923243d31312f624b8
Atta ur Rehman <attaurr786@outlook.com>
2023-08-12 14:51:53 +0500

    hero structure created

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function Hero() {
+  return (
+    <section className="hero-wrapper">
+        <div className="paddings innerWidth flexCenter hero-container">
+            <div className="hero-right">
+               righjt section
+            </div>
+
+            <div className="hero-left">
+
+            </div>
+        </div>
+    </section>
+  )
+}
+
+export default Hero
